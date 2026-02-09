8a29038eb78f96f0982671ecce988de531245142
ALVAROPIOS <40148175+AeroAlvaro@users.noreply.github.com>
2026-02-09 17:40:21 +0100

    Autenticación y Autorización

---
+++ AutenticaciónYAutorización/src/main/java/com/agenda/agenda/repositorios/UsuarioRepository.java
@@ -0,0 +1,22 @@
+package com.agenda.agenda.repositorios;
+
+import com.agenda.agenda.entidades.Rol;
+import com.agenda.agenda.entidades.Usuario;
+import com.agenda.agenda.seguridad.PasswordEncryptor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+
+    public List<Usuario> getUsuarios() {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("aitor", PasswordEncryptor.encrypt("1234"), Rol.ADMIN));
+
+        usuarios.add(new Usuario("alicia", PasswordEncryptor.encrypt("1111"), Rol.USER));
+
+        return usuarios;
+    }
+}
